b2cd7d4b4889f713984625df199f4bee704989f5
akr3ch <akrech.official@gmail.com>
2022-01-24 01:36:32 -0500

    easy to install

--- install.sh
+++ install.sh
@@ -1,40 +1,34 @@
 sudo apt-get install zenity -y
-sudo apt-get install mdk3 -y
-sudo apt-get install aircrack-ng -y
-sudo apt-get install hping3 -y
-sudo apt-get install xterm -y
+
 chmod +x ./wicker
+
 sudo cp ./wicker /usr/bin/wicker
 (
 
 echo 25
 
- echo “EXTRACTING PACKAGES”
+sudo apt-get install mdk3 -y
 
- sleep 1
 
 echo 40
 
- echo “CREATING SHORTCUTS”
+sudo apt-get install aircrack-ng -y
 
- sleep 1
 
 echo 50
 
-echo “ASSIGNING FOLDERS”
+sudo apt-get install hping3 -y
 
- sleep 1
 
 echo 70
 
- sleep 1
+sudo apt-get install xterm -y
+
 
 echo 85
 
- sleep 1
+sleep 1
 
 echo 100
 
- echo “DONE”
-
-) | zenity --title 'INSTALLATION IN PROGRESS' --progress --width=450 && zenity --info --title='akrecH' --text='Installation complete.\nNow you can run wicker just by entering "wicker" from anywhere on your terminal.\n Happy hunting :D' --width=350
+) | zenity --title 'INSTALLATION IN PROGRESS' --progress --auto-kill --auto-close --width=450 && zenity --info --title='akrecH' --text='Installation complete.\nNow you can run wicker just by entering "wicker" from anywhere on your terminal.\n Happy hunting :D' --width=350
